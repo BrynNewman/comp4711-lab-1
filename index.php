4842247e4f5adf514917515f73342fa514f5f991
Bryn <Bryn@hp.hitronhub.home>
2017-01-15 15:22:15 -0800

    Updated the index page

--- index.php
+++ index.php
@@ -12,7 +12,35 @@ and open the template in the editor.
     <body>
         <?php
             include('student.php');
-            
+            $students = array();
+            $first = new Student();
+            $first->surname = "Doe";
+            $first->first_name = "John";
+            $first->add_email('home','john@doe.com');
+            $first->add_email('work','jdoe@mcdonalds.com');
+            $first->add_grade(65);
+            $first->add_grade(75);
+            $first->add_grade(55);
+            $students['j123'] = $first;
+            $second = new Student();
+            $second->surname = "Einstein";
+            $second->first_name = "Albert";
+            $second->add_email('home','albert@braniacs.com');
+            $second->add_email('work1','a_einstein@bcit.ca');
+            $second->add_email('work2','albert@physics.mit.edu');
+            $second->add_grade(95);
+            $second->add_grade(80);
+            $second->add_grade(50);
+            $students['a456'] = $second;
+            $third = new Student();
+            $third->surname = "Newman";
+            $third->first_name = "Bryn";
+            $third->add_email('home','bryn.newman@hotmail.com');
+            $third->add_email('work','bryn.m.newman@gmail.com');
+            $third->add_grade(65);
+            $third->add_grade(85);
+            $third->add_grade(75);
+            ksort($students); // one of the many sort functions
             foreach ($students as $student){
                 echo $student->toString();
             }
