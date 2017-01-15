3c5697ccd383ab2a34bfe4e4ccf7adce527577af
Bryn <Bryn@hp.hitronhub.home>
2017-01-15 13:37:51 -0800

    Added Student Class

--- Student.php
+++ Student.php
@@ -14,8 +14,8 @@
 class Student {
     
     function _construct() {
-        $this->surname = ' ';
-        $this->first_name = ' ';
+        $this->surname = '';
+        $this->first_name = '';
         $this->emails = array();
         $this->grades = array();
     }
@@ -30,17 +30,19 @@ class Student {
     
     function average() {
         $total = 0;
-        foreach  ($this->grades as $value)
+        foreach  ($this->grades as $value){
             $total += $value;
+        }
         return $total / count($this->grades);
     }
     
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
-        $result .= ' (' . $this->average() . ")\n";
-        foreach ($this->emails as $which => $what)
+        $result .= ' ('.$this->average().")\n";
+        foreach ($this->emails as $which => $what){
             $result .= $which . ': ' . $what . "\n";
+        }
         $result .= "\n";
-        return '<pre>' . $result . '</pre>';
+        return '<pre>'.$result.'</pre>';
     }
 }
